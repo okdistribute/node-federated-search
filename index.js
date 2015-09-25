0c8a34f41eeb4729058de7564adfa92dc4723d7c
Karissa McKelvey <krmckelv@gmail.com>
2015-09-25 13:45:56 -0700

    use correct impl of abstract-search api

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function (query, searchers, cb) {
   var streams = []
   for (var i in searchers) {
     var searcher = searchers[i]
-    var stream = searcher(query).pipe(through.obj(function (data, enc, next) {
+    var stream = searcher.stream(query).pipe(through.obj(function (data, enc, next) {
       var res = {}
       res.searcher = {
         name: searcher.name,
