935aa9600b204f4d7dc096c1edadaaaaf8d4b100
Karissa McKelvey <krmckelv@gmail.com>
2015-09-29 01:41:36 -0700

    fix bugs, use parallel

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-var parallel = require('run-parallel')
-var MultiStream = require('multistream')
+var parallel = require('parallel-multistream')
 var through = require('through2')
 
 module.exports = function (query, searchers, cb) {
   var streams = []
   for (var i in searchers) {
-    var searcher = searchers[i]
-    var stream = searcher.stream(query).pipe(through.obj(function (data, enc, next) {
-      var res = {}
-      res.searcher = {
-        name: searcher.name,
-        url: searcher.url,
-        version: searcher.version
-      }
-      res.data = data
-      next(null, res)
-    }))
-    streams.push(stream)
+    (function (i) {
+      var searcher = searchers[i]
+      var stream = searcher.stream(query).pipe(through.obj(function (data, enc, next) {
+        var res = {}
+        res.searcher = {
+          name: searcher.name,
+          url: searcher.url,
+          version: searcher.version
+        }
+        res.data = data
+        next(null, res)
+      }))
+      streams.push(stream)
+    })(i)
   }
-  return MultiStream.obj(streams)
+  return parallel.obj(streams)
 }
